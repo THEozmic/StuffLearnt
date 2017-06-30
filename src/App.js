1f65542ef8c57c32531965f53a42bbdf1aa8f039
THEozmic <michaelozoemena2@gmail.com>
2017-06-30 09:44:04 +0100

    Add Day Component and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './css/App.css';
 import Navbar from './components/Navbar';
+import Day from './components/Day';
 
 export default class App extends React.Component {
     constructor () {
         super();
+        this.state = {
+            userName: null,
+            password: null
+        }
     }
 
     render () {
         return (
             <div className='app'>
                 <Navbar/>
+                <Day/>
             </div>
         )
     }
---
+++ src/components/Day.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import StuffLearnt from './StuffLearnt';
+import jQuery from 'jquery';
+import '../css/Day.css';
+
+export default class Day extends Component {
+    constructor () {
+        super();
+        this.state = {stuff_learnt : []}
+    }
+
+     componentWillMount () {
+        this._fetchStuffLearnt();
+    }
+
+    render () {
+
+        let stuff_learnt = this.state.stuff_learnt.map((day) => {
+            console.log(day);
+            let day_content = day.stuff.map((day_stuff) => {
+                return (<div className="day" key={day_stuff.id}>{day_stuff.content_title}</div>)
+            });
+
+            return (
+                <div className="day-group">
+                    <div className="day-group-date">{day.details.date}</div>
+                    {day_content}
+                </div>
+            )
+    
+        });
+
+        return (
+            <div className="days">
+                {stuff_learnt}
+            </div>
+        )
+    }
+
+    _getStuffLearnt () {
+        return this.state.stuff_learnt.map((stuff) => {
+            return (<div className='day' key={stuff.stuff.content_title}>
+                Hello {stuff.stuff.content_title}
+            </div>)
+        });
+    }
+
+    _fetchStuffLearnt () {
+        jQuery.ajax({
+            method: 'GET',
+            url: '/api/stuff_learnt.json',
+            success: (stuff_learnt) => {
+                this.setState({ stuff_learnt });
+            }
+        });
+    }
+
+}
